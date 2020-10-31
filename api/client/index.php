f7771596fd04460d9cb089a5e655820172737305
guiduan <254732500@qq.com>
2020-10-31 21:27:53 +0800

    Update index.php

--- api/client/index.php
+++ api/client/index.php
@@ -2,3 +2,8 @@
 namespace SakuraPanel;
 
 use SakuraPanel;
+$pm = new SakuraPanel\ProxyManager();
+$userToken=$_GET['token']
+$rs = Database::querySingleLine("tokens", ["token" => $userToken]);
+
+var_dump($rs);
