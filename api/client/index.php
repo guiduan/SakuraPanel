c2c7d2665d4cc014a7002f344f3a6615aa65aa34
guiduan <254732500@qq.com>
2020-10-31 22:07:36 +0800

    Update index.php

--- api/client/index.php
+++ api/client/index.php
@@ -21,7 +21,7 @@ $token = $_GET['token'];
 $sel_server = $_GET['server'];
 $rs = Database::querySingleLine("tokens", ["token" => $token]);
 if($rs) {
-  $pm->getUserProxiesConfig($rs['username'], $sel_server);
+  $pm->getUserProxiesConfig($rs['username'], '3');
 }
 //$sel_server=$_GET['server']
 
